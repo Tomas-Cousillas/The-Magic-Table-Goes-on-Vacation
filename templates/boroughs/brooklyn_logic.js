4ed711ac7b5ed096b46377d2063096924bee0508
shargr2 <shaenahargreaves@gmail.com>
2019-11-09 11:35:41 -0500

    new index

---
+++ templates/boroughs/brooklyn_logic.js
@@ -0,0 +1,84 @@
+// An array of cities and their locations
+var boroughs = [
+  {
+    name: "Brooklyn",
+    location: [40.6782, -73.9442]
+  }
+];
+
+// An array which will be used to store created cityMarkers
+var boroughsMarkers = [];
+
+for (var i = 0; i < boroughs.length; i++) {
+  // loop through the cities array, create a new marker, push it to the cityMarkers array
+  boroughsMarkers.push(
+    L.marker(boroughs[i].location).bindPopup("<h1>" + boroughs[i].name + "</h1>")
+  );
+}
+
+// Add all the cityMarkers to a new layer group.
+// Now we can handle them as one group instead of referencing each individually
+var boroughsLayer = L.layerGroup(boroughsMarkers);
+
+// Define variables for our tile layers
+
+var all_listings = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.dark",
+  accessToken: API_KEY
+});
+
+var short_stays  = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.dark",
+  accessToken: API_KEY
+});
+
+
+var long_stays  = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.dark",
+  accessToken: API_KEY
+});
+
+var private_room  = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.dark",
+  accessToken: API_KEY
+});
+
+var private_house  = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.dark",
+  accessToken: API_KEY
+});
+
+// Only one base layer can be shown at a time
+var baseMaps = {
+  All: all_listings,
+  ShortStays: short_stays,
+  LongStays: long_stays,
+  PrivateRoom: private_room,
+  House: private_house
+};
+
+// Overlays that may be toggled on or off
+var overlayMaps = {
+  Boroughs: boroughsLayer
+};
+
+// Create map object and set default layers
+var myMap = L.map("map", {
+  center: [40.6782, -73.9442],
+  zoom: 12,
+  layers: [all_listings, boroughsLayer]
+});
+
+// Pass our map layers into our layer control
+// Add the layer control to the map
+L.control.layers(baseMaps, overlayMaps).addTo(myMap);
