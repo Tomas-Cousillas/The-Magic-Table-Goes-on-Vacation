5dfe80cb0a9130f6b45ea385d71dba666f9cbe7e
shargr2 <shaenahargreaves@gmail.com>
2019-11-09 13:27:20 -0500

    app update

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-import os
-
-import pandas as pd
-import numpy as np
-
+# Dependencies
+# ----------------------------------
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
+import pandas as pd
 
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
@@ -18,17 +16,22 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 #CONFIGURE DATABASE:
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/FINALdb.sqlite"
-db = SQLAlchemy(app)
+# Create Database Connection
+# ----------------------------------
+engine = create_engine(f"sqlite:///db/FINALdb.sqlite")
+conn = engine.connect()
+# Query All Records in the the Database
+dataframe = pd.read_sql("SELECT * FROM AB_NYC_2019", conn)
+#transform the datafframe into a json object
+#filter json objects into boroughs
 
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(db.engine, reflect=True)
 
-# Save references to each table
-listings_table = Base.classes.keys()
-print(listings_table)
+#BrooklynDF = dataframe.loc[data["neighbourhood_group"] == "Brooklyn"]
+#QueensDF = dataframe.loc[data["neighbourhood_group"] == "Queens"]
+#ManhattanDF = dataframe.loc[data["neighbourhood_group"] == "Manhattan"]
+#StatenDF = dataframe.loc[data["neighbourhood_group"] == "Staten Island"]
+#BronxDF = dataframe.loc[data["neighbourhood_group"] == "Bronx"]
+
 
 @app.route("/")
 def index():
@@ -36,7 +39,25 @@ def index():
     return render_template("index.html")
 
 #Add other routes here:
+@app.route("/Brooklyn")
+def Brooklynplot():
+    graphplot(BrooklynDF)
+    return render_template("brooklyn.html")
 
+@app.route("/Queens")
+def Queensplot():
+    graphplot(QueensDF)
+    return render_template("queens.html")
+
+@app.route("/Manhattan")
+def Manplot():
+    graphplot(ManhattanDF)
+    return render_template("manhattan.html")
+
+@app.route("/Bronx")
+def Bronxplot():
+    graphplot(bronxDF)
+    return render_template("bronx.html")
 
 if __name__ == "__main__":
     app.run()
