d2608736aa921e2e358499331dbec955ec9dbd29
Ann Cascella <ajcascella@gmail.com>
2019-11-15 13:07:43 -0500

    js with heatmap and color corrected popups plus leaflet heat added AJC

--- static/js/queens_logic.js
+++ static/js/queens_logic.js
@@ -1,103 +1,109 @@
-// An array of cities and their locations
-var boroughs = [
-  {
-    name: "Queens",
-    location: [40.7282, -73.7949]
-  }
-];
+console.log("queens js")
 
-fetch("http://127.0.0.1:5000/Queens",{
+// Initialize an array to hold airbnb markers and entire house markers
+var airbnbMarkers = [];
+var entireHouseMarkers = [];
+var heatArray = [];
+
+// An array of cities and their locations
+function createMarkers(response) {
+
+  // Filter by "Entire home/apt"
+  var filteredEntireHouseResponse = response.filter((entry => entry.room_type === "Entire home/apt"))
+  console.log(filteredEntireHouseResponse.length)
+  // Loop through the airbnb array
+  for (var i = 0; i < filteredEntireHouseResponse.length; i++) {
+    if (i < 50) {
+      var airbnb = filteredEntireHouseResponse[i];
+      //For each airbnb, create a marker and bind a popup with the airbnbs name and price
+      var airbnbMarker = L.marker([airbnb.latitude, airbnb.longitude]).bindPopup("<h3> <font color= black>" + airbnb.name + "</font> <h3>" + "<h3> <font color= black> Price: $" + airbnb.price + " per night" + "</font><h3>");
+      //Add the marker to the airbnbMarkers array
+      entireHouseMarkers.push(airbnbMarker);
+    }
+  }
+  // Loop through the airbnb array
+  for (var i = 0; i < response.length; i++) {
+    if (i < 200) {
+      var airbnb = response[i];
+      //For each airbnb, create a marker and bind a popup with the airbnbs name and price
+      var airbnbMarker = L.marker([airbnb.latitude, airbnb.longitude]).bindPopup("<h3> <font color= black>" + airbnb.name + "</font> <h3>" + "<h3> <font color= black> Price: $" + airbnb.price + " per night" + "</font><h3>");
+      //Add the marker to the airbnbMarkers array
+      airbnbMarkers.push(airbnbMarker);
+    }
+  }
+// Create heat map
+    for (var i = 0; i < response.length; i++) {
+      var airbnb = response[i];
+      heatArray.push([airbnb.latitude, airbnb.longitude]);
+    }
+};
+  
+fetch("http://127.0.0.1:5000/Queens", {
   "Access-Control-Allow-Origin": "*"
 })
-  .then( data =>  {
+  .then(data => {
     return data.json()
   })
   .then(function (data) {
-    console.log(data);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  .then( function (data) {
-    //ADD MARKER CODE HERE:
-    //reference listings_list as "data"
+    // console.log(data);
+    createMarkers(data)
 
-// An array which will be used to store created cityMarkers
-var boroughsMarkers = [];
+    // Define variables for our tile layers
+    var emptyGroup = L.layerGroup([])
+    var allListingGroup = L.layerGroup(airbnbMarkers)
+    var entireHouseGroup = L.layerGroup(entireHouseMarkers)
+    var heatGroup = L.layerGroup(heat)
 
-for (var i = 0; i < boroughs.length; i++) {
-  // loop through the cities array, create a new marker, push it to the cityMarkers array
-  boroughsMarkers.push(
-    L.marker(boroughs[i].location).bindPopup("<h1>" + boroughs[i].name + "</h1>")
-  );
-}
+    var allListingsLayer = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "mapbox.dark",
+      accessToken: API_KEY
+    });
 
-// Add all the cityMarkers to a new layer group.
-// Now we can handle them as one group instead of referencing each individually
-var boroughsLayer = L.layerGroup(boroughsMarkers);
+    var entireHouseLayer = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "mapbox.dark",
+      accessToken: API_KEY
+    });
 
-// Define variables for our tile layers
+    var heat_Layer = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+      maxZoom: 18,
+      id: "mapbox.streets",
+      accessToken: API_KEY
+    });
 
-var all_listings = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.dark",
-  accessToken: API_KEY
+    var heat = L.heatLayer(heatArray, {
+      radius: 20,
+      blur: 35
+    }).addTo(heatGroup);
+
+    // Only one base layer can be shown at a time
+    var baseMaps = {
+      none: emptyGroup
+    };
+
+    // Overlays that may be toggled on or off
+    var overlayMaps = {
+      All: allListingGroup,
+      EntireHouse: entireHouseGroup,
+      Heat: heatGroup
+    };
+
+    // Create map object and set default layers
+    var myMap = L.map("map", {
+      center: [40.6782, -73.9442],
+      zoom: 12,
+      layers: [allListingsLayer, entireHouseLayer, heat_Layer]
+    });
+
+    // Pass our map layers into our layer control
+    // Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+    
+
+//   .catch (function (error) {
+//   console.log(error);
 });
-
-var short_stays  = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.dark",
-  accessToken: API_KEY
-});
-
-
-var long_stays  = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.dark",
-  accessToken: API_KEY
-});
-
-var private_room  = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.dark",
-  accessToken: API_KEY
-});
-
-var private_house  = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.dark",
-  accessToken: API_KEY
-});
-
-// Only one base layer can be shown at a time
-var baseMaps = {
-  All: all_listings,
-  ShortStays: short_stays,
-  LongStays: long_stays,
-  PrivateRoom: private_room,
-  House: private_house
-};
-
-// Overlays that may be toggled on or off
-var overlayMaps = {
-  Boroughs: boroughsLayer
-};
-
-// Create map object and set default layers
-var myMap = L.map("map", {
-  center: [40.7282, -73.7949],
-  zoom: 12,
-  layers: [all_listings, boroughsLayer]
-});
-
-// Pass our map layers into our layer control
-// Add the layer control to the map
-L.control.layers(baseMaps, overlayMaps).addTo(myMap);
-
-  });
-  
